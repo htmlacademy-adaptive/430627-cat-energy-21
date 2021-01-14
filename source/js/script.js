89dc328c6e057ce90b11637791e48c53ab9c5ee8
Skadis <68993615+Skadis@users.noreply.github.com>
2021-01-14 21:22:47 +0300

    Подготовка проекта перед сдачей

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+//Меню
+const menuButton = document.querySelector(".main-menu__toggle");
+
+console.log(menuButton);
+
+if ((!menuButton.classList.contains("main-menu__toggle--close")) && (!menuButton.classList.contains("main-menu__toggle--open"))) {
+  menuButton.classList.add("main-menu__toggle--open");
+}
+
+menuButton.addEventListener("click", function (evt) {
+  if (menuButton.classList.contains("main-menu__toggle--close")) {
+    menuButton.classList.add("main-menu__toggle--open");
+    menuButton.classList.remove("main-menu__toggle--close");
+  } else {
+    menuButton.classList.add("main-menu__toggle--close");
+    menuButton.classList.remove("main-menu__toggle--open");
+  }
+});
+
